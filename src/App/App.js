69601e80d552dcefee7767e749a691d54ebde082
Arden Ranta <arranta20@gmail.com>
2024-01-11 10:13:16 -0800

    feat/add Spells component

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+// import Card from '../Card/Card.js';
+import Spells from '..Spells/Spells.js';
 import './App.css';
 
 function App() {
+
+  const [spells, setSpells] = useState([])
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>spellkeeper</h1>
       </header>
       <main className='main-view'>
-        <section className='spell-list'></section>
+        <section className='spell-list'>
+          <Spells />
+        </section>
       </main>
       <footer>
         <button>Your Spellbook</button>
@@ -19,6 +26,6 @@ function App() {
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
